7991297eb073e98875e9c8dd1cb713a217cabaf1
Bant3r241 <bobby36ko@outlook.com>
2025-08-10 15:07:03 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+if game.PlaceId == 75418531859354 then
+    local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+    local Window = OrionLib:MakeWindow({Name="ABI │ Catch A Brainrot",HidePremium=false,IntroEnabled=false,IntroText="ABI",SaveConfig=true,ConfigFolder="XlurConfig"})
+
+
+
+
+
+
+
+
+
+
+
+  --Tabs
+  local MainTab = Window:MakeTab({Name="Main",Icon="rbxassetid://4299432428",PremiumOnly=false})
