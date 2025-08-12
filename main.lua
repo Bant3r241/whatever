b6c8e3e6defbd3851746946faed903b4a1b6ad98
Bant3r241 <bobby36ko@outlook.com>
2025-08-11 22:56:23 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,12 @@
-if game.PlaceId == 75418531859354 then
+if game.PlaceId == 128120317905952 then
     local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
-    local Window = OrionLib:MakeWindow({Name="ABI │ Catch A Brainrot",HidePremium=false,IntroEnabled=false,IntroText="ABI",SaveConfig=true,ConfigFolder="XlurConfig"})
+    local Window = OrionLib:MakeWindow({Name="ABI │ Click To Aura Farm", HidePremium=false, IntroEnabled=false, IntroText="ABI", SaveConfig=true, ConfigFolder="XlurConfig"})
 
+    _G.AutoHit = false
+    function autoHit() while _G.AutoHit do game:GetService("ReplicatedStorage").src.Packages.Knit.Services.GameService.RE.HandleClick:FireServer(3) task.wait(0.1) end end
 
+    local MainTab = Window:MakeTab({Name="Main", Icon="rbxassetid://4299432428", PremiumOnly=false})
+    MainTab:AddToggle({Name="Auto Hit", Default=false, Callback=function(v) _G.AutoHit = v if v then autoHit() end end})
+end
 
-
-
-
-
-
-
-
-
-  --Tabs
-  local MainTab = Window:MakeTab({Name="Main",Icon="rbxassetid://4299432428",PremiumOnly=false})
+OrionLib:Init()
