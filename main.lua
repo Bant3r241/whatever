b5c79a779b647fdc3de409179f975090ac6a683f
Bant3r241 <bobby36ko@outlook.com>
2025-08-12 21:56:24 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 90462358603255 then
     local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
-    local Window = OrionLib:MakeWindow({Name="ABI │ Anime Eternal", HidePremium=false, IntroEnabled=false, IntroText="ABI", SaveConfig=true, ConfigFolder="XlurConfig"})
+    local Window = OrionLib:MakeWindow({Name="ABI │ Anime Eternal",HidePremium=false,IntroEnabled=false,IntroText="ABI",SaveConfig=true,ConfigFolder="XlurConfig"})
 
     local AutoFarm = Window:MakeTab({Name="AutoFarm", Icon="rbxassetid://4299432428", PremiumOnly=false})
 
